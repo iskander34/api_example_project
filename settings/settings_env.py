2b68d08b91b57ddd2fa896aa1b5899d109926209
iskander34 <@iskander34>
2025-04-01 22:58:19 +0300

    add_basic_logic

---
+++ settings/settings_env.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="config.env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="."
+    )
+    base_url: str
+
+    @property
+    def api_url(self) -> str:
+        return self.base_url
+
+
+base_settings = Settings()
